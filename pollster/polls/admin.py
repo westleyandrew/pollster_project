fd10bb411a920f117c63ef50610e7ad509a1951a
Westley Andrew <westleyandrew@hotmail.com>
2019-09-23 15:03:17 +0100

    working on views

--- pollster/polls/admin.py
+++ pollster/polls/admin.py
@@ -4,3 +4,21 @@ from __future__ import unicode_literals
 from django.contrib import admin
 
 # Register your models here.
+from .models import Question, Choice
+
+admin.site.site_header = "Pollster Admin"
+admin.site.site_title = "Pollster Admin Area"
+admin.site.index_title = "Welcome to the Pollster Admin Area"
+
+class ChoiceInline(admin.TabularInline):
+  model = Choice
+  extra =3
+
+class QuestionAdmin(admin.ModelAdmin):
+  fieldsets = [(None, {'fields': ['question_text']}),
+  ('Date Information', {'fields': ['pub_date'], 'classes': ['collapse']}),]
+  inlines = [ChoiceInline]
+
+# admin.site.register(Question)
+# admin.site.register(Choice)
+admin.site.register(Question, QuestionAdmin)
--- pollster/polls/views.py
+++ pollster/polls/views.py
@@ -4,3 +4,8 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 
 # Create your views here.
+# Get questions and display them
+def index(request):
+  latest_question_list = Question.objects.order_by('-pub_date')[:5]
+  context = {'latest_question_list: latest_question_list'}
+  return render(request, 'polls/index.html', context)
